ae299ed3d82dc027b3b2647f5b3194040065d140
DRACula <c.daedalus@wustl.edu>
2024-08-13 14:54:09 -0500

    added faq

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,11 +1,86 @@
 <template>
-
+ <div class="faq-list">
+    <h1 class="faq-text-1">FAQ</h1>
+    <faqItem
+      v-for="(faq, index) in faqs"
+      :key="index"
+      :faq="faq"
+    />
+  </div>
 </template>
 
 <script>
-
+    import faqItem from "./faqItem.vue";
+    export default {
+        components: {
+            faqItem,
+        },
+        data() {
+            return {
+                faqs: [
+                    {
+                        question: 'What is a hackathon?',
+                        answer: '...',
+                    },
+                    {
+                        question: 'When is HackWashU?',
+                        answer: 'bruh you don\'t even know when the hackathon is?',
+                    },
+                    {
+                        question: 'who can attend?',
+                        answer: 'idk... but not you',
+                    },
+                    {
+                        question: 'What should I bring?',
+                        answer: 'Your mom...',
+                    },
+                    {
+                        question: 'is HackWashU in-person?',
+                        answer: 'You think we\'d let you in person?',
+                    },
+                    {
+                        question: 'is HackWashU free?',
+                        answer: 'Pay up',
+                    },
+                    {
+                        question: 'If I can\'t code, should I still come?',
+                        answer: 'No',
+                    },
+                    {
+                        question: 'Will there be swag?',
+                        answer: 'You\'ll get a sticker',
+                    },
+                    {
+                        question: 'i have more questions',
+                        answer: 'too bad',
+                    }
+                ],
+            };
+        },
+    };
 </script>
 
 <style scoped>
+.faq-list{
+    position: relative;
+    height: auto;
+    margin-bottom: 4vw;
+    display: flex;
+    width: 100vw;
+    align-self: center;
+    flex-direction: column;
+}
+
+.faq-text-1{
+    width: 100vw;
+    position: relative;
+    text-align: center;
+    font-family: 'FancyFont', serif;
+    font-size: 6vw;
+    display: flex;
+    justify-content: center; 
+    align-items: center; 
+    margin-bottom: 2vw;
+}
 
 </style>
---
+++ src/components/faqItem.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="faq-item">
+      <button @click="toggle" class="faq-question">
+        {{ faq.question }}
+        <span>{{ isOpen ? '-' : '+' }}</span>
+      </button>
+      <div v-if="isOpen" class="faq-answer">
+        {{ faq.answer }}
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      faq: Object,
+    },
+    data() {
+      return {
+        isOpen: false,
+      };
+    },
+    methods: {
+      toggle() {
+        this.isOpen = !this.isOpen;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .faq-item {
+    /* margin-bottom: 0.3vw; */
+    margin-left:30vw;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    position: relative;
+    width: 32vw;
+    flex-direction: column;
+    font-family: 'spartan-sb', serif;
+  }
+  
+  .faq-question {
+    background-color: transparent;
+    border: none;
+    padding: 10px;
+    text-align: left;
+    color: #ffffff;
+    font-size: 1.8vw;
+    width: 100%;
+    cursor: pointer;
+  }
+  
+  .faq-answer {
+    padding: 10px;
+    background-color: transparent;
+    border-left: 3px solid #dbdbdb;
+    text-align: left;
+    width: 100%;
+    display: flex;
+  }
+  </style>
+  
