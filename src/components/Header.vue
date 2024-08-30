3e48c8375c688ef63420607e4b90e277857ec7b4
Skyturtl <112505166+Skyturtl@users.noreply.github.com>
2024-08-30 03:00:50 -0500

    Finished Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 
 <template>
 <div class="header">
-  <div class="logo"></div>
+  <img class="logo" src="../assets/hacklogo.png">
   <div class="header-right">
     <a href="#">ABOUT</a>
     <a href="#">TRACKS</a>
@@ -11,6 +11,7 @@
     <a href="#">CONTACT</a>
     <a href="#" class="apply">APPLY</a>
   </div>
+  <img class="mobile" src="../assets/hamicon.png">
 </div>
 </template>
 
@@ -33,13 +34,12 @@
   text-decoration: none;
 }
 
-.header div.logo{
+.header img.logo{
   float: left;
   height: 4.5vw;
   width: 4.5vw;
   margin-left:10vw;
   margin-right:15vw;
-  background-color: gray;
 }
 
 .header a:hover {
@@ -60,4 +60,117 @@
   align-items: center;
 }
 
+.mobile{
+  display: none;
+}
+
+@media (max-width: 768px) {
+  .header {
+    overflow: hidden;
+    background-color: #C5D2CE;
+    border-bottom: solid 0.75vw black;
+    display: flex;
+    align-items:center;
+  }
+
+  .header a {
+    font-size: 5.5vw;
+    font-family: 'pf-videotext';
+    color: black;
+    text-align: center;
+    text-decoration: none;
+    display: none;
+  }
+
+  .header img.logo{
+    float: left;
+    height: 20vw;
+    width: 20vw;
+    margin-top: -2vw;
+    margin-bottom: -2vw;
+    margin-left: 6vw;
+    margin-right: 21vw;
+  }
+
+  .header a:hover {
+    text-decoration: underline;
+    transition: ease-in 0.5s;
+  }
+
+  .header a.apply {
+    background-color: #64D3F5;
+    border: .75vw solid black;
+    padding: 3.25vw 5vw;
+    display: block;
+  }
+
+  .header-right {
+    float: right;
+    display: flex;
+    align-items: center;
+  }
+
+  .mobile{
+    display: block;
+    height: 15vw;
+    width: 15vw;
+    padding-left: 4vw;
+    padding-top: 3vw;
+  }
+}
+
+@media (max-width: 480px) {
+  .header {
+    overflow: hidden;
+    background-color: #C5D2CE;
+    border-bottom: solid 0.75vw black;
+    display: flex;
+    align-items:center;
+  }
+
+  .header a {
+    font-size: 5.5vw;
+    font-family: 'pf-videotext';
+    color: black;
+    text-align: center;
+    text-decoration: none;
+    display: none;
+  }
+
+  .header img.logo{
+    float: left;
+    height: 20vw;
+    width: 20vw;
+    margin-top: -2vw;
+    margin-bottom: -2vw;
+    margin-left: 6vw;
+    margin-right: 21vw;
+  }
+
+  .header a:hover {
+    text-decoration: underline;
+    transition: ease-in 0.5s;
+  }
+
+  .header a.apply {
+    background-color: #64D3F5;
+    border: .75vw solid black;
+    padding: 3.25vw 5vw;
+    display: block;
+  }
+
+  .header-right {
+    float: right;
+    display: flex;
+    align-items: center;
+  }
+
+  .mobile{
+    display: block;
+    height: 15vw;
+    width: 15vw;
+    padding-left: 4vw;
+    padding-top: 3vw;
+  }
+}
 </style>
